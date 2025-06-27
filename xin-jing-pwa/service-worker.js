7aae317fdedead748fd58dca6df7e619475d65f7
蟑螂恶霸 <291015944@qq.com>
2025-06-27 20:53:00 +0800

    Add files via upload

---
+++ xin-jing-pwa/service-worker.js
@@ -0,0 +1,25 @@
+
+const CACHE_NAME = 'xin-jing-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// 安装并缓存资源
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// 拦截请求
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
